68cab2ccc6eec605c86eddb4d17e78c4b98ac841
Wayne Chen <wchen206@my.bcit.ca>
2023-10-31 15:29:01 -0700

    added firebase file and notification page

---
+++ scripts/firebaseAPI_TEAM19.js
@@ -0,0 +1,18 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+  apiKey: "AIzaSyBBRxD13drIdaGLi0Qd0HKfB4WR9K4Gm2E",
+  authDomain: "comp1800-bby19.firebaseapp.com",
+  projectId: "comp1800-bby19",
+  storageBucket: "comp1800-bby19.appspot.com",
+  messagingSenderId: "836877814838",
+  appId: "1:836877814838:web:e4a64c0bbce2fed23ddc97"
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
