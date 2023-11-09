46ffa87847bfef4db5ae6d39ab1746c265ec4c8d
Clement Quach <clementcnd@gmail.com>
2023-11-09 11:24:04 -0800

    fixing issues with merging on accountv2

--- scripts/accountsv2.js
+++ scripts/accountsv2.js
@@ -11,27 +11,25 @@ function populateUserInfo() {
                     currentUser.get()
                         .then(userDoc => {
                             //get the data fields of the user
-<<<<<<< HEAD
+
                             var userName = userDoc.data().name;
                             // var userSchool = userDoc.data().school;
                             // var userCity = userDoc.data().city;
-=======
-                            var userName = userDoc.name;
->>>>>>> a9dc6d7076179f4f90c95e812aa2063a5cf173d6
+
 
                             //if the data fields are not empty, then write them in to the form.
                             if (userName != null) {
                                 document.getElementById("nameInput").value = userName;
                             }
-<<<<<<< HEAD
+
                             // if (userSchool != null) {
                             //     document.getElementById("schoolInput").value = userSchool;
                             // }
                             // if (userCity != null) {
                             //     document.getElementById("cityInput").value = userCity;
                             // }
-=======
->>>>>>> a9dc6d7076179f4f90c95e812aa2063a5cf173d6
+
+
                         })
                 } else {
                     // No user is signed in.
@@ -43,7 +41,7 @@ function editUserInfo() {
           //Enable the form fields
           document.getElementById('personalInfoFields').disabled = false;
        }
-<<<<<<< HEAD
+
 function saveUserInfo() {
         //enter code here
    
@@ -63,23 +61,6 @@ function saveUserInfo() {
         //c) disable edit 
         document.getElementById('personalInfoFields').disabled = true;
       }
-=======
 
-function saveUserInfo() {
-    //enter code here
-
-    //a) get user entered values
-    userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    //b) update user's document in Firestore
-    currentUser.update({
-        name: userName,
-    })
-    .then(() => {
-        console.log("Document successfully updated!");
-    })
-    //c) disable edit 
-    document.getElementById('personalInfoFields').disabled = true;
-}
->>>>>>> a9dc6d7076179f4f90c95e812aa2063a5cf173d6
 //call the function to run it 
 populateUserInfo();
