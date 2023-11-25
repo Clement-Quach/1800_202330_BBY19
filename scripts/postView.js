b389373e38f7a69342a877ca66d32da4b7f2bec4
bluedelaw <daniel.law1128@gmail.com>
2023-11-24 19:09:43 -0800

    changed postview to include profile pics

--- scripts/postView.js
+++ scripts/postView.js
@@ -12,48 +12,67 @@ document.addEventListener('DOMContentLoaded', () => {
   auth.onAuthStateChanged((user) => {
     if (user) {
       const postRef = db.collection('discussionSubmissions').doc(documentSubmissionID || postId);
-
+  
       postRef.onSnapshot((doc) => {
         if (doc.exists) {
           const postData = doc.data();
+          const postUserID = postData.userID; // Assuming postData contains the userID
+        
+          // Get the user document based on the post's userID
+          const userRef = db.collection('users').doc(postUserID);
+        
+          // Retrieve the user data and handle it accordingly
+          userRef.get().then((userDoc) => {
+            if (userDoc.exists) {
+              const userData = userDoc.data();
+              let profilePic = userData.profilePic; // Assuming profilePic exists in userData
 
-          // Construct the HTML string with the post creator's name from postData
-          const htmlString = `
-            <div class="title">${postData.title}</div>
-            <div class="container">
-              <div class="post">
-                <div id="userImage">
-                  <img src="./images/userIcon.jpg">
+              // Check if profilePic exists and is not empty/null/undefined
+              if (!profilePic) {
+                // If profilePic doesn't exist or is empty, use a default user icon
+                profilePic = './images/userIcon.jpg';
+              }
+
+              // Construct the HTML string with the post creator's name from postData
+              const htmlString = `
+                <div class="title">${postData.title}</div>
+                <div class="container">
+                  <div class="post">
+                    <div id="userImage">
+                      <img src="${profilePic}">
+                    </div>
+                    <div id="userInfo">${userData.name}</div> <!-- Assuming name is in the user data -->
+                    <div id="discussion">${postData.details}</div>
+                  </div>
+                  <div class="comment">
+                    <div id="commentMessage">LEAVE A COMMENT</div>
+                    <div id="commentImage">
+                      <img src="${profilePic}"> <!-- Using the same imgSrc for comment image -->
+                    </div>
+                    <div id="commentName">${user.displayName}</div> <!-- Display the logged-in user's name -->
+                    <div id="commentBox">
+                      <textarea id="commentInput" placeholder="Comment here"></textarea>
+                    </div>
+                    <div id="commentPost">
+                      <input id="commentPreview" type="button" value="PREVIEW">
+                      <input id="commentPostComment" type="button" value="POST COMMENT">
+                    </div>
+                  </div>
                 </div>
-                <div id="userInfo">${postData.name}</div> <!-- Use postData.name for post creator's name -->
-                <div id="discussion">${postData.details}</div>
-              </div>
-              <div class="comment">
-                <div id="commentMessage">LEAVE A COMMENT</div>
-                <div id="commentImage">
-                  <img src="./images/userIcon.jpg">
-                </div>
-                <div id="commentName">${user.displayName}</div> <!-- Display the logged-in user's name -->
-                <div id="commentBox">
-                  <textarea id="commentInput" placeholder="Comment here"></textarea>
-                </div>
-                <div id="commentPost">
-                  <input id="commentPreview" type="button" value="PREVIEW">
-                  <input id="commentPostComment" type="button" value="POST COMMENT">
-                </div>
-              </div>
-            </div>
-          `;
-          // Set the constructed HTML string to the postContentDiv
-          postContentDiv.innerHTML = htmlString;
+              `;
+              // Set the constructed HTML string to the postContentDiv
+              postContentDiv.innerHTML = htmlString;
+            } else {
+              console.error('User document does not exist');
+            }
+          }).catch((error) => {
+            console.error('Error getting user document:', error);
+          });
         } else {
           console.error('Post document does not exist');
         }
-      }, (error) => {
-        console.error('Error fetching document: ', error);
       });
     } else {
-      // User is not logged in
       console.error('User is not logged in');
     }
   });
