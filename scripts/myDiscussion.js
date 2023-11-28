4abfcc2ffa402d41c45bc0ed18b093aa3e5b2653
bluedelaw <daniel.law1128@gmail.com>
2023-11-28 10:39:52 -0800

    removed svg from myDiscussion

--- scripts/myDiscussion.js
+++ scripts/myDiscussion.js
@@ -94,9 +94,6 @@ function fetchDataAndDisplay(userID, sort, order) {
             <span class="tag tag-teal" id="title">${status}</span>
             <span class="tag tag-purple" id="title">${data.concern}</span>
             <span class="tag tag-pink" id="title">${data.location}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots" viewBox="0 0 16 16" onclick="toggleOptions('${comment.commentID}')">
-              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3"/>
-            </svg>
           </div>
           <div class="card-body">
             <div class="user">
